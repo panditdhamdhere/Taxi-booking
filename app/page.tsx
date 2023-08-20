b809db032c089f7f68497d05d45bb25e6aba3f2f
panditdhamdhere <dhamdherepandit@gmail.com>
2023-08-20 09:14:38 +0530

    booking page building

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-     
-      <h2>Pandit</h2>
+    <div className="">
+     <div className="grid grid-cols-1 md:grid-cols-3">
+<div className="bg-blue-100">
+bookiing
+</div>
+
+<div className="col-span-2 bg-red-100 "> 
+{/* order-first md:order-last */}
+map
+</div>
+     </div>
+    
     </div>
   );
 }
